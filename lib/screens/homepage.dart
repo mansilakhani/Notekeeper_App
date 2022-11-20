42556c0d90be92d50cca1e591869eabc17e9ac56
mansilakhani <111499824+mansilakhani@users.noreply.github.com>
2022-11-21 00:41:59 +0530

    Update homepage.dart

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,10 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fm_notes_app/screens/notes_editor.dart';
-import 'package:fm_notes_app/screens/notes_reader.dart';
-import 'package:fm_notes_app/widgets/note_card.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import '../helpers/cloud_firestore_helper.dart';
+import '../style/app_style.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,13 +14,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  static GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  static TextEditingController titleController = TextEditingController();
+  static TextEditingController contextController = TextEditingController();
+
+  String title = "";
+  String content = "";
+
+  // int color_id = Random().nextInt(AppStyle().cardsColor.length);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+     
       backgroundColor: Colors.black,
-      //backgroundColor: AppStyle.mainColor,
       appBar: AppBar(
-        // elevation: 0,
+       
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -43,7 +52,7 @@ class _HomePageState extends State<HomePage> {
         // backgroundColor: AppStyle.mainColor,
       ),
       body: Padding(
-        padding: EdgeInsets.all(18),
+        padding: const EdgeInsets.all(18),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -56,58 +65,184 @@ class _HomePageState extends State<HomePage> {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(
-              height: 20,
-            ),
+            const SizedBox(height: 20),
             Expanded(
               child: StreamBuilder<QuerySnapshot>(
-                  stream: FirebaseFirestore.instance
-                      .collection("Notes")
-                      .snapshots(),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    if (snapshot.hasData) {
-                      return ListView(
-                        // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        //     crossAxisCount: 2),
-                        children: snapshot.data!.docs
-                            .map((note) => noteCard(() {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              NoteReader_Page(doc: note)));
-                                }, note))
-                            .toList(),
-                      );
-                    }
-                    return Text(
-                      "There is no Notes",
-                      style: GoogleFonts.nunito(color: Colors.white),
+                stream:
+                    CloudFirestoreHelper.cloudFirestoreHelper.selectRecord(),
+                builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
                     );
-                  }),
-            )
+                  }
+                  if (snapshot.hasData) {
+                    QuerySnapshot? data = snapshot.data;
+                    List<QueryDocumentSnapshot> list = data!.docs;
+                    return ListView.builder(
+                        itemCount: list.length,
+                        itemBuilder: (context, i) {
+                          return Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Card(
+                              elevation: 3,
+                              color: AppStyle.cardsColor[list[i]['color_id']],
+                              child: ListTile(
+                                //leading: Text("${i + 1}"),
+                                title: Text(
+                                  "${list[i]['note_title']}",
+                                  style: AppStyle.mainTitle,
+                                ),
+                                subtitle: Text("${list[i]['note_content']}"),
+
+                               
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    IconButton(
+                                      onPressed: () {
+                                        updateData(id: list[i].id);
+                                      },
+                                      icon: const Icon(
+                                        Icons.edit,
+                                        color: Colors.blue,
+                                      ),
+                                    ),
+                                    IconButton(
+                                        onPressed: () async {
+                                          await CloudFirestoreHelper
+                                              .cloudFirestoreHelper
+                                              .deleteRecord(
+                                            id: list[i].id,
+                                          );
+                                        },
+                                        icon: const Icon(
+                                          Icons.delete,
+                                          color: Colors.red,
+                                        )),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        });
+                  }
+                  return Text(
+                    "There is no Notes",
+                    style: GoogleFonts.nunito(color: Colors.white),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Color(0xfffcca3f),
+        backgroundColor: const Color(0xfffcca3f),
         onPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(
               // builder: (context) => NoteEditorScreen(),
-              builder: (context) => NoteEditor_Page(),
+              builder: (context) => const NoteEditor_Page(),
             ),
           );
         },
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
+    );
+  }
+
+  updateData({required String id}) async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Form(
+            key: formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Center(
+                  child: Text("Update"),
+                ),
+                TextFormField(
+                  controller: titleController,
+                  onSaved: (val) {
+                    title = val!;
+                  },
+                  validator: (val) {
+                    (val!.isEmpty) ? 'Enter your title first...' : null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: "Title",
+                    label: Text("Enter Your title"),
+                  ),
+                ),
+                TextFormField(
+                  controller: contextController,
+                  onSaved: (val) {
+                    content = val!;
+                  },
+                  validator: (val) {
+                    (val!.isEmpty) ? 'Enter your context first' : null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: "Context",
+                    label: Text("Enter Your context"),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () async {
+                        Navigator.of(context).pop();
+                        if (formKey.currentState!.validate()) {
+                          formKey.currentState!.save();
+
+                          Map<String, dynamic> update = {
+                            'note_title': title,
+                            'note_content': content,
+                          };
+
+                          CloudFirestoreHelper.cloudFirestoreHelper
+                              .updateRecord(id: id, updateData: update);
+
+                          titleController.clear();
+                          contextController.clear();
+
+                          setState(() {
+                            title = "";
+                            content = "";
+                          });
+                        } else {
+                          Navigator.of(context).pop();
+                        }
+                      },
+                      child: const Text("Update"),
+                    ),
+                    OutlinedButton(
+                      onPressed: () {
+                        titleController.clear();
+                        contextController.clear();
+
+                        setState(() {
+                          title = "";
+                          content = "";
+                        });
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text("Cancel"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
